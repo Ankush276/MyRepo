577983dce3bac94cb53aefd828282d70e8f7828a
Ankush276 <kumarankush9696@gmail.com>
2023-04-13 15:04:40 +0530

    Initial

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+  supabaseUrl: 'https://szxroegupnnioenohgfo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6eHJvZWd1cG5uaW9lbm9oZ2ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODA4Njk5MzQsImV4cCI6MTk5NjQ0NTkzNH0.d0uknzj66E9deo-h4816vGnnJh89_OYXRurWyHJipAw',
+};
